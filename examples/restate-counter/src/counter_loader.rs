3ee40859b13e1cca53db99655717152d4f418616
Kevin O'Neill <kevin@oneill.id.au>
2026-05-04 12:35:56 +1000

    Simplify Restate counter store wiring

--- examples/restate-counter/src/counter_loader.rs
+++
@@ -1,8 +0,0 @@
-use wee_events::{Aggregate, AggregateId};
-
-pub trait CounterLoader: Send + Sync {
-    fn load_counter(
-        &self,
-        id: &AggregateId,
-    ) -> impl std::future::Future<Output = wee_events::Result<Aggregate>> + Send;
-}
--- examples/restate-counter/src/main.rs
+++ examples/restate-counter/src/main.rs
@@ -1,23 +1,16 @@
 mod commands;
-mod counter_environment;
-mod counter_loader;
 mod counter_publisher;
 mod events;
 mod randomiser;
 mod services;
-mod sqlite_counter_loader;
-mod sqlite_counter_publisher;
 mod state;
 mod system_randomiser;
 
 use std::sync::Arc;
 
-use counter_environment::CounterEnvironment;
 use restate_sdk::prelude::*;
 use services::audit_log::{AuditLog, AuditLogImpl};
 use services::counter::{CounterService, CounterServiceBinder};
-use sqlite_counter_loader::SqliteCounterLoader;
-use sqlite_counter_publisher::SqliteCounterPublisher;
 use system_randomiser::SystemRandomiser;
 use wee_events_sqlite::{GlobalStrategy, SqliteEventStore};
 
@@ -33,16 +26,14 @@ async fn main() {
             .await
             .expect("event store should open"),
     );
-    let loader = SqliteCounterLoader::new(Arc::clone(&store));
-    let publisher = SqliteCounterPublisher::new(store);
     let randomiser = SystemRandomiser;
-    let environment = CounterEnvironment::new(loader, publisher, randomiser);
 
     HttpServer::new(
         Endpoint::builder()
             .bind(
                 wee_events_restate::create(CounterService)
-                    .with_env(environment)
+                    .with_store(store)
+                    .with_env(randomiser)
                     .serve(),
             )
             .bind(AuditLogImpl.serve())
